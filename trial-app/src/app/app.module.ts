b1ab570cceb4f58290259df05f48a4844efcb129
marlon.calleja <marlon.calleja@brightstarr.com>
2020-08-06 14:43:06 +0200

    Added PWA settings.

--- trial-app/src/app/app.module.ts
+++ trial-app/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { CommentComponent } from './social-comments/comment.component';
 import { EventComponent } from './social-comments/event.component';
 import { RainbowDirective } from './test-components/rainbow/rainbow.directive';
 import { LifecycleComponent } from './test-components/lifecycle/lifecycle.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import { LifecycleComponent } from './test-components/lifecycle/lifecycle.compon
     EventComponent,   
     RainbowDirective, LifecycleComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   entryComponents: [CommentComponent, EventComponent],
   providers: [SocialService],
   bootstrap: [AppComponent],
