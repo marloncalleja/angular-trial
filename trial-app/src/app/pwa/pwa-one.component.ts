3ea4e146cbcdd03fb5bff7e629fdcacbc3a21a68
marlon.calleja <marlon.calleja@brightstarr.com>
2020-08-10 10:51:56 +0200

    Changed PWA component manifest route.

--- trial-app/src/app/pwa/pwa-one.component.ts
+++ trial-app/src/app/pwa/pwa-one.component.ts
@@ -14,69 +14,9 @@ export class PwaOneComponent implements OnInit {
   }
 
   loadManifest() {
-    var myDynamicManifest = {
-      "name": "pwa-one",
-      "short_name": "pwa-one",
-      "theme_color": "#000000",
-      "background_color": "#ffffff",
-      "display": "standalone",
-      "scope": "/",
-      "start_url": "/",
-      "icons": [
-        {
-          "src": "assets/icons/icon-72x72.png",
-          "sizes": "72x72",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-96x96.png",
-          "sizes": "96x96",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-128x128.png",
-          "sizes": "128x128",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-144x144.png",
-          "sizes": "144x144",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-152x152.png",
-          "sizes": "152x152",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-384x384.png",
-          "sizes": "384x384",
-          "type": "image/png",
-          "purpose": "maskable any"
-        },
-        {
-          "src": "assets/icons/icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "maskable any"
-        }
-      ]
-    }
-    const stringManifest = JSON.stringify(myDynamicManifest);
-    const blob = new Blob([stringManifest], { type: 'application/json' });
-    const manifestURL = URL.createObjectURL(blob);
-    document.querySelector('#my-manifest-placeholder').setAttribute('href', manifestURL);
+  
+    document.querySelector('#my-manifest-placeholder').setAttribute('href', 'assets/manifest-test.json');
+
   }
 
 }
